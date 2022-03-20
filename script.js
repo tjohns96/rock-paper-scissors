5389d4bc800c738aa8b615de84902842f1f9bd98
Trevor Johnson <trevorjohnson9601@gmail.com>
2022-03-20 17:20:20 -0400

    Add functionality to evauluate the winner of a game and also to play a game

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ function computerPlay() {
             return "Paper";
     }
 }
+
+function playGame(userChoice, computerChoice = computerPlay()) {
+    console.log(evaluateWinner(userChoice, computerChoice));
+}
+
+function evaluateWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return (`Tie game! You both chose ${userChoice}, so no winner.`);
+    }
+    else {
+        if (userChoice === "Paper" && computerChoice === "Scissors") {
+            return ("Scissors beats paper! You lost.");
+        }
+        else if (userChoice === "Rock" && computerChoice === "Paper") {
+            return ("Paper beats rock! You lost.");
+        }
+        else if (userChoice === "Scissors" && computerChoice === "Rock") {
+            return ("Rock beats scissors! You lost.");
+        }
+
+        else {
+            return (`${userChoice} beats ${computerChoice}! You won.`)
+        }
+    }
+
+}
+
+playGame("Rock");
+playGame("Paper");
+playGame("Scissors");
+playGame("Scissors");
